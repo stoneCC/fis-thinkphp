056f8833a5e283f84a9d3d1ccfd57672588dca14
stone chang <changchuan@yaolan.com>
2015-09-02 14:30:13 +0800

    no message

--- runtime/Cache/Home/416b8d4df02c9ebb9390278dad68298d.php
+++ runtime/Cache/Home/416b8d4df02c9ebb9390278dad68298d.php
@@ -6,7 +6,7 @@
     <p>Notebook is designed by <a rel="nofollow" href="http://www.tooplate.com">Free HTML Templates</a>. Credits go to <a rel="nofollow" href="http://www.photovaco.com">Free Photos</a> for photos, <a rel="nofollow" href="http://jwloh.deviantart.com/art/Aquaticus-Social-91014249">jwloh</a> for Aquaticus social icons, and <a rel="nofollow" href="http://www.icojoy.com">icojoy.com</a> for icons used in this template. Morbi rutrum euismod elit, nec adipiscing ante sodales sed. Cras accumsan congue turpis a luctus.</p>
   </div>
   <div class="content_section last_section">
-    <h2>Our Services</h2>
+    <h2><?php echo ($tmplvar["info"]["addr"]); ?></h2>
     <ul class="service_list">
       <li><a href="#" class="service_one">Maecenas suscipit vulputate dui vel adipiscing</a></li>
       <li><a href="#" class="service_two">Pellentesque habitant morbi tristique senectus</a></li>
